2349ad6dc350ad3a0ccc39523e8af11e35d4015e
Abhishek <abhishekchouhannk@gmail.com>
2023-05-12 22:18:29 -0700

    updated gpt.js to utilize the custom module

--- gpt.js
+++ gpt.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require("./custom/openai");
 
 const app = express();
 
